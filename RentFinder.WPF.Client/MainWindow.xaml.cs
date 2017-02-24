35118ed4c47afc87c8b478983010b931273f70f2
vboz <vboz@isddesign.com>
2017-02-24 15:55:37 +0200

    add wpf app to review results

--- RentFinder.WPF.Client/MainWindow.xaml.cs
+++ RentFinder.WPF.Client/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace RentFinder.WPF.Client
 {
@@ -10,6 +13,17 @@ namespace RentFinder.WPF.Client
         public MainWindow()
         {
             InitializeComponent();
+            listbox1.SelectionChanged += (sender, args) => browser.Load(listbox1.SelectedValue.ToString());
+        }
+
+        private void RichTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            listbox1.Items.Clear();
+            var links = (sender as TextBox).Text.Split(new [] { "\n" }, StringSplitOptions.None).ToList();
+            foreach (var link in links)
+            {
+                listbox1.Items.Add(link);
+            }
         }
     }
 }
